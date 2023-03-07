2890b76f8aa1da033ae0c3e9d16d1ef7138f2ecd
david halison <halison700@gmail.com>
2023-03-06 21:56:57 -0300

    Ajustes no projeto

--- run-docker-test.sh
+++
@@ -1,5 +0,0 @@
-docker-compose up -d influxdb grafana
-echo "--------------------------------------------------------------------------------------"
-echo "Load testing with Grafana dashboard http://localhost:3000/d/k6/k6-load-testing-results"
-echo "--------------------------------------------------------------------------------------"
-docker-compose run --rm -v $PWD/:/scripts k6 run /scripts/main.js
